cda91c6cb6cc77a60d3936f9c1cdbf19b3117c0f
sakria9 <i@sustech.moe>
2023-02-02 22:58:50 +0800

    fix: use sbrk

--- src/cgen/stdlib/strcat.c
+++ src/cgen/stdlib/strcat.c
@@ -7,12 +7,12 @@ struct str_object {
 };
 
 struct str_object* strcat(struct str_object* s1, struct str_object* s2) {
-    struct str_object* ret = malloc(s1->object_size * 4);
+    struct str_object* ret = sbrk(s1->object_size * 4);
     ret->class_tag = s1->class_tag;
     ret->object_size = s1->object_size;
     ret->dispatch_table = s1->dispatch_table;
     ret->length = s1->length + s2->length;
-    ret->value = malloc(ret->length + 1);
+    ret->value = sbrk(ret->length + 1);
     for (int i = 0; i < s1->length; i++) {
         ret->value[i] = s1->value[i];
     }
